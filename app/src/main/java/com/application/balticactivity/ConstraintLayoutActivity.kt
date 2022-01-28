a3b0f07c26f1c2baf0e1dcdb76b988978b57ba23
Калистый Никита <prostonikita8848@gmail.com>
2022-01-28 21:14:00 +0200

    constraintLayout 2, bariers, guideLines, groups

--- app/src/main/java/com/application/balticactivity/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/application/balticactivity/ConstraintLayoutActivity.kt
@@ -2,6 +2,8 @@ package com.application.balticactivity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_constraint_layout.*
@@ -12,8 +14,16 @@ class ConstraintLayoutActivity: AppCompatActivity() {
         setContentView(R.layout.activity_constraint_layout)
 
         toggleLastNameButton.setOnClickListener{
-            lastName.isVisible = !lastName.isVisible
+            userInfoGroup.isVisible = !userInfoGroup.isVisible
         }
+
+        userInfoGroup.referencedIds.forEach{ id ->
+            findViewById<View>(id).setOnClickListener{
+                Toast.makeText(this, "clicked on view $id in group", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
 
     }
 }
